64f244dc2c9a45564ca81be5893de8eea653df0d
Femi Akinyemi <santosfemakin@yahoo.com>
2021-04-24 20:56:26 +0100

    third commit

--- news-app/src/config.js
+++ news-app/src/config.js
@@ -1,4 +1,4 @@
 // Replace the string with your private API key for the News API.
 // You can get one for free if you register at: https://newsapi.org/register
 
-export const NEWS_API_KEY = "YOUR_PRIVATE_API_KEY";
+export const NEWS_API_KEY = "c7c5be46bc3842c9a01bdd32783dc6cb";
